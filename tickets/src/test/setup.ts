fbbfe3f17e9e03a940cea9fc446d9c2706a5f6ce
Marko Misura <marko.misura@toptal.com>
2023-10-20 18:44:17 +0200

    Implemented the update router and its tests for the tickets microservice

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -36,7 +36,7 @@ afterAll(async () => {
 global.signin = () => {
     // Build a JWT payload. { id, email }
     const payload = {
-        id: 'asdfsdsda',
+        id: new mongoose.Types.ObjectId().toHexString(),
         email: 'test@test.com'
     };
 
