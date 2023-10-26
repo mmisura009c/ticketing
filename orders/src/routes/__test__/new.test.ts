84284b3186c5d99d8d88365bf88156c4e6195068
Marko Misura <marko.misura@toptal.com>
2023-10-26 19:27:45 +0200

    Setup the test suite for the order microservice, written some unit tests for the new router

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+import { OrderStatus } from '@lafmmticketing/common';
+
+it('returns an error if the ticket does not exist', async () => {
+    const ticketId = new mongoose.Types.ObjectId();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId })
+        .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const order = Order.build({
+        ticket,
+        userId: 'asdf',
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id })
+        .expect(400);
+});
+
+it('reserves a ticket', async () => {
+    let orders = await Order.find({});
+    expect(orders.length).toEqual(0);
+
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({
+            ticketId: ticket.id
+        })
+        .expect(201);
+
+    orders = await Order.find({});
+    expect(orders.length).toEqual(1);
+});
