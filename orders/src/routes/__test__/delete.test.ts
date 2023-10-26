ab3f946498295e3fd3ebdd39a1189dd93b43a3c5
Marko Misura <marko.misura@toptal.com>
2023-10-26 20:51:57 +0200

    Added an implementation for cancelling an order, also written unit tests for it

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,81 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+import mongoose from 'mongoose';
+
+it('returns an error if an invalid orderId is sent', async () => {
+    const fakeOrderId = 'fake id';
+
+    // Make a request to cancel the order
+    await request(app)
+        .delete(`/api/orders/${fakeOrderId}`)
+        .set('Cookie', global.signin())
+        .send()
+        .expect(400);
+});
+
+it('returns an error if order does not exist', async () => {
+    const fakeOrderId = new mongoose.Types.ObjectId().toHexString();
+
+    // Make a request to cancel the order
+    await request(app)
+        .delete(`/api/orders/${fakeOrderId}`)
+        .set('Cookie', global.signin())
+        .send()
+        .expect(404);
+});
+
+it('returns an error if a user tries to cancel some elses order', async () => {
+    // Create a ticket with Ticket Model
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const user = global.signin();
+    // Make a request to create an order
+    const { body: order } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    // Make a request to cancel the order
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', global.signin())
+        .send()
+        .expect(401);
+});
+
+it('marks an order as cancelled', async () => {
+    // Create a ticket with Ticket Model
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const user = global.signin();
+    // Make a request to create an order
+    const { body: order } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    // Make a request to cancel the order
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(204);
+
+    // Expectation to make sure the thing is cancelled
+    const updatedOrder = await Order.findById(order.id);
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits a order cancelled event');
