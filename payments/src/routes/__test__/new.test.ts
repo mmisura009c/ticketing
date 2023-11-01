7418ba8a2f7537ef40e606a7b3b9120dfc637396
Marko Misura <marko.misura@toptal.com>
2023-11-01 18:17:42 +0100

    Initial stripe implementation for charging done, also written unit test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import { OrderStatus } from '@lafmmticketing/common';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
+
+jest.mock('../../stripe');
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
     await request(app)
@@ -54,4 +57,30 @@ it('returns a 400 when purchasing a cancelled order', async () => {
             orderId: order.id,
         })
         .expect(400);
+});
+
+it('returns a 204 with valid inputs', async () => {
+    const userId = new mongoose.Types.ObjectId().toHexString();
+    const order = Order.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        userId: userId,
+        status: OrderStatus.Created,
+        price: 10,
+        version: 0,
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin(userId))
+        .send({
+            token: 'tok_visa', // Always successful stripe test token
+            orderId: order.id,
+        })
+        .expect(201);
+
+    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+    expect(chargeOptions.source).toEqual('tok_visa');
+    expect(chargeOptions.amount).toEqual(order.price * 100);
+    expect(chargeOptions.currency).toEqual('usd');
 });
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -8,6 +8,7 @@ import {
     NotAuthorizedError,
     OrderStatus
 } from '@lafmmticketing/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -40,7 +41,13 @@ router.post('/api/payments',
             throw new BadRequestError('Cannot pay for an cancelled order');
         }
 
-        res.send({ success: true });
+        await stripe.charges.create({
+            currency: 'usd',
+            amount: order.price * 100,
+            source: token
+        });
+
+        res.status(201).send({ success: true });
     }
 );
 
